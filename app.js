63ca8ebe7b0069dba702dc94e974dc23b4d48836
inkinvissible <agusmartin.13.113@gmail.com>
2024-12-13 11:52:24 -0300

    Ejercicio 4.14

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const blogRouter = require('./controllers/blog')
+const usersRouter = require('./controllers/users')
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
+const {errorHandler} = require('./utils/middleware')
 
 mongoose.set('strictQuery', false)
 mongoose.connect(config.MONGODB_URI)
@@ -20,6 +22,10 @@ app.use(express.static('dist'))
 app.use(express.json())
 
 
+
 app.use('/api/blogs', blogRouter)
+app.use('/api/users', usersRouter)
+
+app.use(errorHandler)
 
 module.exports = app
