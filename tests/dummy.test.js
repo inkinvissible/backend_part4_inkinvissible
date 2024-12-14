ec7522380abb002b16aa6a070e701c5db35e0cda
inkinvissible <agusmartin.13.113@gmail.com>
2024-12-14 12:46:57 -0300

    Ejercicios con Lodash, 4.3 - 4.7

--- tests/dummy.test.js
+++ tests/dummy.test.js
@@ -8,3 +8,56 @@ test('dummy returns one', () => {
   const result = listHelper.dummy(blogs)
   assert.strictEqual(result, 1)
 })
+
+test('the most liked author is Anna Gety', () => {
+  const initialBlogs = [
+    {
+      "title": "Comprar Bitcoins",
+      "author": "Satoshi Nakamoto",
+      "url": "https://google.com",
+      "likes": 3
+    },
+    {
+      "title": "La importancia del console.log() en Web Development",
+      "author": "Anna Gety",
+      "url": "https://google.com",
+      "likes": 5
+    },
+    {
+      "title": "La importancia del Web Development",
+      "author": "Anna Gety",
+      "url": "https://google.com",
+      "likes": 3
+    }
+  ]
+
+  const mostLikedAuthor = listHelper.mostLikes(initialBlogs)
+
+  assert.strictEqual(mostLikedAuthor.author, "Anna Gety")
+})
+
+test('the most blogs are from Anna Gety', () => {
+  const initialBlogs = [
+    {
+      "title": "Comprar Bitcoins",
+      "author": "Satoshi Nakamoto",
+      "url": "https://google.com",
+      "likes": 3
+    },
+    {
+      "title": "La importancia del console.log() en Web Development",
+      "author": "Anna Gety",
+      "url": "https://google.com",
+      "likes": 5
+    },
+    {
+      "title": "La importancia del Web Development",
+      "author": "Anna Gety",
+      "url": "https://google.com",
+      "likes": 3
+    }
+  ]
+
+  const mostBlogsAuthor = listHelper.mostBlogs(initialBlogs)
+  assert.strictEqual(mostBlogsAuthor.author, "Anna Gety")
+})
--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,3 +1,4 @@
+const _ = require('lodash')
 const dummy = (blogs) => {
     return 1
 }
@@ -6,15 +7,48 @@ const totalLikes = (blogs) => {
     return blogs.length === 0 ? 0 : blogs.reduce((total, num) => total + num.likes, 0)
 }
 
-const favoriteBlog = (blogs) =>{
-    if (blogs.length === 0){ return null}
-
+const favoriteBlog = (blogs) => {
+    if (blogs.length === 0) { return null }
     return blogs.reduce((max, blog) => blog.likes > max.likes ? blog : max, blogs[0])
+}
 
+const mostLikes = (blogs) => {
+    const groupedAuthors = _.groupBy(blogs, 'author')
+    
+    const totalLikesAuthor = _.mapValues(groupedAuthors, (blog, author) =>({
+        author: author,
+        likes: _.sumBy(blog, 'likes')
+    }))
+    const totalLikesArray = _.map(totalLikesAuthor, (value, key) => ({
+        author: key,
+        likes: value.likes
+    }))
+    
+    const mostLikedAuthor = _.maxBy(totalLikesArray, 'likes')
+    return mostLikedAuthor
+}
+
+const mostBlogs = (blogs) => {
+    const groupedAuthors = _.groupBy(blogs, 'author')
+    
+    const totalBlogsAuthor = _.mapValues(groupedAuthors, (blog, author) =>({
+        author: author,
+        blogs: blog.length
+    }))
+
+    const totalBlogsArray = _.map(totalBlogsAuthor, (value, key) => ({
+        author: key,
+        blogs: value.blogs
+    }))
+
+    const mostBlogsAuthor = _.maxBy(totalBlogsArray, 'blogs')
+    return mostBlogsAuthor
 }
 
 module.exports = {
     dummy,
     totalLikes,
-    favoriteBlog
+    favoriteBlog,
+    mostLikes,
+    mostBlogs
 }
